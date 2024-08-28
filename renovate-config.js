997ba04552708fb76d0f693551aeb4c93f42cc5a
Vikas Pandey <vikas110@gmail.com>
2024-08-28 07:01:17 +0200

    Update renovate-config.js

--- renovate-config.js
+++ renovate-config.js
@@ -6,9 +6,9 @@ module.exports = {
   repositories: ['hclpandv/k8s-lab', 'hclpandv/my-vagrant-projects'],
   onboardingConfig: {
     "$schema": "https://docs.renovatebot.com/renovate-schema.json",
-    "extends": [
-      "github>apter-tech/renovate-config#v0.2.0"
-    ]
+    //"extends": [
+    //  "github>apter-tech/renovate-config#v0.2.0"
+    //]
   },
   platform: 'github',
   forkProcessing: "enabled",
